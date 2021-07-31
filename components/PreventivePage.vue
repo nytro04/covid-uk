0c09aa4fd934bff7e1f1178b84c43395ccd0cc4e
Francis Badasu <nytro04@gmail.com>
2021-07-31 14:29:56 +0000

    feat: add prevent measures page

---
+++ components/PreventivePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="preventive container m-auto sm:px-10">
+    <div class="md:grid gap-10 grid-cols-1 md:grid-cols-3 text-center my-40">
+      <div class="">
+        <h3 class="text-2xl font-bold mb-4">WASH HANDS</h3>
+        <p class="text-lg">
+          Keep washing your <br />
+          hands regularly
+        </p>
+      </div>
+      <div class="">
+        <h3 class="text-2xl font-bold mb-4">COVER FACE</h3>
+        <p class="text-lg">
+          Wear a face covering <br />
+          in enclosed spaces
+        </p>
+      </div>
+      <div class="">
+        <h3 class="text-2xl font-bold mb-4">MAKE SPACE</h3>
+        <p class="text-lg">
+          Stay at least 2 metres <br />
+          apart, wear face covering
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.preventive {
+  /* height: 50vh; */
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
   <div class="">
     <Hero />
     <StatsPage />
+    <ChartLine />
+    <PreventivePage />
   </div>
 </template>
 
@@ -10,6 +12,8 @@ export default {
    components: {
     Hero: () => import('~/components/HeroPage.vue'),
     StatsPage: () => import('~/components/StatsPage.vue'),
+    ChartLine: () => import('~/components/ChartLine.vue'),
+    PreventivePage: () => import('~/components/PreventivePage.vue'),
    }
 }
 </script>
