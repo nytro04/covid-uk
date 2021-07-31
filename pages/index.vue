77d497648bb3eee275eabf39a7d5c7d043733d2c
Francis Badasu <nytro04@gmail.com>
2021-07-31 18:39:03 +0000

    feat: success making api request

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="">
     <Hero />
+    <!-- <StatsPage :new-cases="newCases" /> -->
     <StatsPage />
     <ChartLine />
     <PreventivePage />
@@ -8,12 +9,68 @@
 </template>
 
 <script>
+// import { mapActions } from 'vuex'
 export default {
    components: {
     Hero: () => import('~/components/HeroPage.vue'),
     StatsPage: () => import('~/components/StatsPage.vue'),
     ChartLine: () => import('~/components/ChartLine.vue'),
     PreventivePage: () => import('~/components/PreventivePage.vue'),
-   }
+   },
+
+   data() {
+     return {
+       covidResult: [],
+       newCases: {},
+       areaType: "nation",
+       areaName: "england",
+       currentPage: 1,
+       structure: {
+           date: "date",
+      name: "areaName",
+      cases: {
+      new: "newCasesByPublishDate",
+      cumulative: "cumCasesByPublishDate"
+    },
+       }
+     }
+   },
+
+   mounted() {
+     this.fetchData()
+   },
+
+   methods: {
+    //  ...mapActions({
+    //     setNewCases: 'setNewCases/cases'
+    //  }),
+     async fetchData(){
+       const filters = [`areaType=${this.areaType}`, `areaName=${this.areaName}`]
+       const apiParams = {
+         filters: filters.join(";"),
+         structure: JSON.stringify(this.structure),
+         latestBy: "newCasesByPublishDate"
+       }
+
+       try {
+
+        //  const res = await this.$axios.$get(`/data?filters=areaType=nation;areaName=england&structure={"date":"date","cumCasesByPublishDate":"cumCasesByPublishDate", "newCases":"newCasesByPublishDate"}`)
+         const res = await this.$axios.$get(`/data?`, {
+           params:String(apiParams)
+         })
+
+
+        //  this.covidResult = res.data
+          this.newCases = res.data[0]
+         console.log(this.newCases, 'res')
+  // this.setNewCases(res.data[0][0])
+
+  console.log(String(apiParams))
+
+       } catch (error) {
+         console.log(error, 'error fetching data')
+       }
+     }
+   },
 }
 </script>
