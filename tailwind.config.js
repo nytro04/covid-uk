2b749eb5071eaea4375b5ddb09ba4639e5837ebf
Francis Badasu <nytro04@gmail.com>
2021-07-31 06:53:17 +0000

    feat: set up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: {
+    mode: 'layers',
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+      // TypeScript
+      'plugins/**/*.ts',
+      'nuxt.config.ts'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.trueGray,
+
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  }
+}
