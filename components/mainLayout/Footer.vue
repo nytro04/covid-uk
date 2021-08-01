54acfb7150dd8015c361326dd6c19c2815ed3d41
Francis Badasu <nytro04@gmail.com>
2021-08-01 17:28:44 +0000

    feat: add responsiveness to footer

--- components/mainLayout/Footer.vue
+++ components/mainLayout/Footer.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="container mx-auto py-12 text-white">
-    <div class="flex justify-between">
+  <div class="container py-12 mx-auto text-white">
+    <div class="block mx-5 space-y-8 md:space-y-0 md:flex md:justify-between">
       <div>
-        <p class="text-lg">An RCCO development challenge by Francis Badasu</p>
+        <p class="mb-3 text-lg md:mb-1">
+          An RCCO development challenge by Francis Badasu
+        </p>
         <p class="text-lg">Data source: Gov.uk</p>
       </div>
       <h3 class="text-xl font-bold">COVID UK</h3>
@@ -11,9 +13,7 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
